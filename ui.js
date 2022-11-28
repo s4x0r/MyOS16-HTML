213160acf466d44c5cbd1f8b7e442d1e41f22f7b
s4x0r <s4x0r.101@gmail.com>
2022-11-27 22:24:04 -0600

    puuuuuush

--- ui.js
+++ ui.js
@@ -1,4 +1,8 @@
-//document.getElementById('display').innerText="true";
+
+window.addEventListener('message', (event) => {         
+    console.log(event.data);
+}, false);
+
 $(document).ready(function() {
     $("#mycolor").colorpicker({
         defaultPalette: 'web'
@@ -11,9 +15,40 @@ $(document).ready(function() {
 });
 
 function send(data){
-    window.parent.postMessage(data, '*');
+    //window.parent.postMessage(data, '*');
     //document.getElementById('display').innerText=data;
     console.log(data)
+
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', window.parent.location.href, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(data); 
+    
+}
+function send(data, callback){
+    console.log(data)
+    var xhr = new XMLHttpRequest();
+    xhr.onload=function(){
+        console.log(xhr.responseText)
+        callback(xhr.responseText)
+    }
+    xhr.open('POST', window.parent.location.href, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(data); 
+    
+}
+function get(data, callback){
+    console.log(data)
+    var xhr = new XMLHttpRequest();
+    xhr.onload=function(){
+        console.log(xhr.responseText)
+        callback(xhr.responseText)
+    }
+    xhr.open('POST', window.parent.location.href, true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.get(data); 
+    
 }
 function show(display){
     for(let i = 0; i<document.getElementsByClassName('screen').length; i++){
